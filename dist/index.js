24660a978d6557ac637698149896187456d6c149
Acaruso <anthonycaruso102@gmail.com>
2024-04-07 15:20:35 -0400

    add files

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayHello = void 0;
+function sayHello() {
+    console.log("Hello, world!");
+}
+exports.sayHello = sayHello;
+//# sourceMappingURL=index.js.map
